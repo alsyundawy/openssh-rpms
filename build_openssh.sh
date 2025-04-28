18db7831dd68a2a4b87d93d346a0c5a82de8db2e
nugent <67910375+nugent1a@users.noreply.github.com>
2025-04-28 11:14:25 +0800

    Update build_openssh.sh

--- build_openssh.sh
+++ build_openssh.sh
@@ -15,7 +15,7 @@ echo '==========================================================================
 #系统版本
 OS=$(grep '^NAME=' /etc/os-release | sed 's/^NAME="\([^"]*\)"/\1/')
 #OpenSSH版本
-OPENSSH_VERSION=9.9p2
+OPENSSH_VERSION=10.0p1
 
 read -p '
 帮助:
